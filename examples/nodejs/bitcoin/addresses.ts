dc2e46aad60fff5c2237cbf68783517e39c3010d
HOBBIT-BrainPay CEO <162181638+Horlabrainmoore@users.noreply.github.com>
2026-01-31 13:17:02 -0500

    Change Bitcoin address and add Bech32 decode function
    Updated Bitcoin address format and added minimal Bech32 decode implementation.

--- examples/nodejs/bitcoin/addresses.ts
+++ examples/nodejs/bitcoin/addresses.ts
@@ -4,7 +4,7 @@ const init = async () => {
   try {
     const { bitcoin: { addresses } } = mempoolJS();
     
-    const address = '1wizSAYSbuyXbt9d8JV8ytm5acqq2TorC';
+    const address = 'bc1qpqlsehzrjmxhutxmlwt6tdjkwafvcgugpv5375';
     
     const myAddress = await addresses.getAddress({ address });
     console.log(myAddress);
@@ -25,3 +25,31 @@ const init = async () => {
   }
 };
 init();
+# Minimal bech32 decode implementation
+CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
+def bech32_decode(bech):
+    bech = bech.lower()
+    pos = bech.rfind('1')
+    hrp = bech[:pos]
+    data = [CHARSET.find(c) for c in bech[pos+1:]]
+    return hrp, data
+
+def convertbits(data, frombits, tobits):
+    acc = 0
+    bits = 0
+    ret = []
+    maxv = (1 << tobits) - 1
+    for value in data:
+        acc = (acc << frombits) | value
+        bits += frombits
+        while bits >= tobits:
+            bits -= tobits
+            ret.append((acc >> bits) & maxv)
+    return ret
+
+addr="bc1qpqlsehzrjmxhutxmlwt6tdjkwafvcgugpv5375"
+hrp,data=bech32_decode(addr)
+witver=data[0]
+prog=convertbits(data[1:],5,8)
+import binascii
+print(binascii.hexlify(bytes(prog)).decode())
